d6e81ba9750b68c9c352fafe4ff457dafcbb456a
Rui Alves <ruialves20010323@gmail.com>
2023-02-25 12:42:12 +0000

    Camera following player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
