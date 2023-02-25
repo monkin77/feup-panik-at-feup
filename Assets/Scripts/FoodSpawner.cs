701fe32817207b1877f33af91a7c49805b4ce993
Rui Alves <ruialves20010323@gmail.com>
2023-02-25 14:54:04 +0000

    Removing food after x seconds passed. Repositioning camera

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -1,38 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FoodSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] foodReference;
     
+    private List<GameObject> spawnedFood;
+
+    [SerializeField] private float SPAWN_INF_TIME = 1f;
+    [SerializeField] private float SPAWN_MAX_TIME = 5f;
+    [SerializeField] private float DESTROY_TIME = 5f;
+    
+    [SerializeField] private float SPAWN_X_MIN = -8f;
+    [SerializeField] private float SPAWN_X_MAX = 8f;
+    [SerializeField] private float SPAWN_Y_MIN = -4f;
+    [SerializeField] private float SPAWN_Y_MAX = 4f;
+
+    private void Awake()
+    {
+        spawnedFood = new List<GameObject>();
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnFood());
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    
     IEnumerator SpawnFood()
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(1, 5));
+            yield return new WaitForSeconds(Random.Range(SPAWN_INF_TIME, SPAWN_MAX_TIME));
             
             int randomIndex = Random.Range(0, foodReference.Length);
             
-            Vector3 position = new Vector3(Random.Range(-8, 8), Random.Range(-4, 4), 0);
+            Vector3 position = new Vector3(Random.Range(SPAWN_X_MIN, SPAWN_X_MAX), 
+                Random.Range(SPAWN_Y_MIN, SPAWN_Y_MAX), 0);
             
-            GameObject spawnedFood = Instantiate(foodReference[randomIndex]);
-            spawnedFood.transform.position = position;
+            GameObject newFood = Instantiate(foodReference[randomIndex]);
+            newFood.transform.position = position;
+            
+            spawnedFood.Add(newFood);
+            StartCoroutine(DestroyFood(newFood));
         }
+    }
+    
+    IEnumerator DestroyFood(GameObject food)
+    {
+        yield return new WaitForSeconds(DESTROY_TIME);
+        spawnedFood.Remove(food);
+        Destroy(food);
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null)
+                {
+                    GameObject go = new GameObject("GameManager");
+                    instance = go.AddComponent<GameManager>();
+                }
+            }
+            return instance;
+        }
+    }
+    
+    [SerializeField]
+    private GameObject playerInstance;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
